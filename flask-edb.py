b1f9e6c475062aab67210d1df301521d91373980
pjh82381425 <87836028+pjh82381425@users.noreply.github.com>
2025-02-04 15:48:30 +0000

    create gitignore & edit flask

--- flask-edb.py
+++ flask-edb.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    data = {"message": "햄부기햄북 햄북어 햄북스딱스 함부르크햄부가우가 햄비기햄부거 햄부가티햄부기온앤 온 을 차려오거라"}
+    return render_template('index.html', data=data)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
