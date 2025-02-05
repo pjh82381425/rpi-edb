68ffea266882217092a4266e551923cd3a387020
pjh82381425 <87836028+pjh82381425@users.noreply.github.com>
2025-02-05 12:37:48 +0900

    예제완성

--- flask-edb.py
+++ flask-edb.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from lib import get_temperature, get_humidity, get_pm25
+from flask import Flask, render_template, jsonify
+import datetime
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = {"message": "햄부기햄북 햄북어 햄북스딱스 함부르크햄부가우가 햄비기햄부거 햄부가티햄부기온앤 온 을 차려오거라"}
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
+@app.route('/get_data')
+def get_data():
+    temperature = get_temperature()
+    humidity = get_humidity()
+    pm25 = get_pm25()
+    
+    # 현재 시간 가져오기
+    current_time = datetime.datetime.now().strftime('%H:%M:%S')
+
+    # 반환할 데이터
+    data = {
+        "temperature": temperature,
+        "humidity": humidity,
+        "pm25": pm25,
+        "current_time": current_time
+    }
+    
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ lib.py
@@ -0,0 +1,7 @@
+def get_temperature():
+    return 11
+def get_humidity():
+    return 11
+def get_pm25():
+    return 11
+    
