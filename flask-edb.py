ec8560746cde289b17f9166f80edb566d91029e1
pjh82381425 <hojin2040290@gmail.com>
2025-02-08 02:42:36 +0900

    .

--- flask-edb.py
+++ flask-edb.py
@@ -41,6 +41,5 @@ def get_data():
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run(host='0.0.0.0', port=5000, threaded=True)   # gpt가 알려준거
-    
+    app.run(host='0.0.0.0', port=5000, threaded=True) # 성능 최적화  
 
