4786f4298c9e1bbf6d64dd48468722e0da6c74db
pjh82381425 <87836028+pjh82381425@users.noreply.github.com>
2025-02-14 10:53:50 +0900

    update serial funtion

--- lib.py
+++ lib.py
@@ -4,19 +4,17 @@ PORT = "COM6"  # 🔥 올바른 포트로 변경하세요.
 BAUDRATE = 9600
 
 ser = serial.Serial(PORT, BAUDRATE, timeout=2)  # timeout 증가
-ser.flushInput()  # 🔥 기존 버퍼 삭제
 
 def read():
-    print(1)
+    ser.flushInput()  # 🔥 기존 버퍼 삭제
     latest_temp, latest_hum = None, None
-    if ser.in_waiting:
-        print(2)
-        line = ser.readline().decode("utf-8").strip()
-        print(f"📡 Raw Data: {line}")  # 🔥 디버깅용 출력
-        if line:
-            data = line.split(",")
-            if len(data) == 2:
-                latest_temp, latest_hum = data  # 최신 값 저장
+    while ser.in_waiting == None:
+        pass
+    line = ser.readline().decode("utf-8").strip()
+    if line:
+        data = line.split(",")
+        if len(data) == 2:
+            latest_temp, latest_hum = data  # 최신 값 저장
     
     ser.close()  # 🔥 포트 닫기
     return latest_temp, latest_hum
